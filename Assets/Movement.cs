ab463be5b416cf3b73367a9c7c5c8dacf0f22014
Melisa <melcafidan@gmail.com>
2022-06-19 17:17:47 +0200

    Walk Animation

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,7 +4,7 @@ public class Movement : MonoBehaviour
 {
     public float speed = 8.0f;
     public float maxRotation = 360;
-    
+
     private Rigidbody rb;
 
     void Start()
@@ -12,23 +12,15 @@ public class Movement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
+        
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
 
-        // if (movement == Vector3.zero)
-        // {
-        //     return;
-        // }
-        //
-        // Quaternion targetRotation = Quaternion.LookRotation(movement);
-        // targetRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, maxRotation * Time.fixedDeltaTime);
-        
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-        // rb.MoveRotation(targetRotation);
+        rb.velocity = movement * (speed * Time.deltaTime);
+        rb.MovePosition(rb.position + movement * (speed * Time.fixedDeltaTime));
     }
 
 }
