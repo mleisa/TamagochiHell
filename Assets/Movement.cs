9d3227974f54ed2eecb2580a5a0875736faf2654
Melisa <melcafidan@gmail.com>
2022-06-19 17:55:38 +0200

    Fix Character Speed

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 8.0f;
+    public float speed = 10.0f;
     public float maxRotation = 360;
 
     private Rigidbody rb;
@@ -18,9 +18,7 @@ public class Movement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
-
-        rb.velocity = movement * (speed * Time.deltaTime);
-        rb.MovePosition(rb.position + movement * (speed * Time.fixedDeltaTime));
+        rb.MovePosition(rb.position + movement * (speed * Time.deltaTime));
     }
 
 }
