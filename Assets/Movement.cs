eda3f5d394b5524e50a2b5a13fed24cb752b740e
Melisa <melcafidan@gmail.com>
2022-06-22 15:13:53 +0200

    Flashlight system

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -7,18 +8,27 @@ public class Movement : MonoBehaviour
 
     private Rigidbody rb;
 
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
         rb.MovePosition(rb.position + movement * (speed * Time.deltaTime));
+
+        // rb.position += verticalInput * transform.forward * Time.deltaTime * speed;
+        // rb.position += horizontalInput* transform.right * Time.deltaTime * speed;
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.velocity = Vector3.zero;
+        Debug.Log(gameObject.name);
+    }
 }
