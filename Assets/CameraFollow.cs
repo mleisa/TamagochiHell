5f074840768395e28593dcee9ec07ca89084569d
Melisa <melcafidan@gmail.com>
2022-06-30 21:24:00 +0200

    Fixes (see PR)

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -10,10 +7,10 @@ public class CameraFollow : MonoBehaviour
     [SerializeField]
     private float offsetX, offsetY, offsetZ;
     
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>();
     }
 
     // Update is called once per frame
