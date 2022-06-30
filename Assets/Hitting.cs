cf51202de3b8056b7af75b81dc81c5c3f8ee6fdf
Dohruba <ricardohendrichs@gmail.com>
2022-06-30 12:42:23 +0200

    Separated Damage enemies hitting and Player attacking

--- Assets/Hitting.cs
+++ Assets/Hitting.cs
@@ -4,50 +4,23 @@ using UnityEngine;
 public class Hitting : MonoBehaviour
 {
     private bool canHitAgain = true;
-    [SerializeField] FightingSphere fightingSphere;
-    private List<GameObject> deadNarcs;
-    [SerializeField]  private float force = 1000;
-
-    private void Awake()
+    private void OnCollisionEnter(Collision collision)
     {
-        fightingSphere = gameObject.GetComponentInChildren<FightingSphere>();
-    }
+        HealthSystem thing = collision.gameObject.GetComponent<HealthSystem>();
 
-    private void FixedUpdate()
-    {
-        if (Input.GetMouseButtonDown(0) && canHitAgain)
+        if (thing != null && canHitAgain)
         {
-            foreach (GameObject narc in fightingSphere.Narcs)
-            {
-                HealthSystem enemyHealth = narc.GetComponent<HealthSystem>();
-                enemyHealth.TakeDamage(100);
-                if (enemyHealth.CurrentHealth <= 5)
-                {
-                    //fightingSphere.removeNarc(narc);
-                    narc.GetComponent<Rigidbody>().AddForce(new Vector3(0, force, 0));
-                    Debug.Log(narc + ": deaaaad");
-                }
-            }
+            thing.TakeDamage(10);
+            canHitAgain = false;
         }
     }
 
-    /*    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionExit(Collision other)
+    {
+        HealthSystem thing = other.gameObject.GetComponent<HealthSystem>();
+        if (thing != null)
         {
-            HealthSystem thing = collision.gameObject.GetComponent<HealthSystem>();
-
-            if (thing != null && canHitAgain)
-            {
-                thing.TakeDamage(10);
-                canHitAgain = false;
-            }
+            canHitAgain = true;
         }
-
-        private void OnCollisionExit(Collision other)
-        {
-            HealthSystem thing = other.gameObject.GetComponent<HealthSystem>();
-            if (thing != null)
-            {
-                canHitAgain = true;
-            }
-        }*/
+    }
 }
